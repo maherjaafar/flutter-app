03435e96e2e33e2dabd19ecc2302f0ff89673c16
maherjaafar <maher.jaafar@hotmail.com>
2021-03-20 22:00:11 +0100

    Implemented models

---
+++ lib/Ui/core/models/user.dart
@@ -0,0 +1,15 @@
+class User {
+  final String phoneNumber;
+  final String firstName;
+  final String lastName;
+  final String userAvatar;
+  final List messages;
+
+  User({
+    this.phoneNumber,
+    this.firstName,
+    this.lastName,
+    this.userAvatar,
+    this.messages,
+  });
+}
